8196555f223f4ce0aa6e0a01717e3c6573e96132
Jose Arroyave <arquos.14@gmail.com>
2023-03-17 10:09:52 -0500

    fix: delete unuse code

--- ecommerce/ecommerce_store/serializers/user_serializer.py
+++ ecommerce/ecommerce_store/serializers/user_serializer.py
@@ -1,25 +1,8 @@
 from rest_framework import serializers
 
 from ecommerce_store.models import EcommerceUser
-
-class EcommerceStoreListingField(serializers.RelatedField):
-    """
-    This class is for serializer relations from user to stores
-    """
-
-    def to_representation(self, value):
-        """overwrite representation ecommerce"""
-        pass
     
 class EcommerceUserSerializer(serializers.ModelSerializer):
-    
-    # def validate_email(self, value):
-    #     print("tipo: ",type(value))
-    #     if not value:
-    #         raise serializers.ValidationError("username value not found")
-    
-    # def validate(self, attrs):
-    #     return attrs
     class Meta:
         model = EcommerceUser
         fields = ('id', 'username', 'email')
